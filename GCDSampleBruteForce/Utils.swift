ec2cf54b7c097cce312e04cf9fe8513e212322e1
Sultan <serioz.jak@gmail.com>
2023-08-18 00:20:45 +0600

    Add stop brute method.

--- GCDSampleBruteForce/Utils.swift
+++ GCDSampleBruteForce/Utils.swift
@@ -17,28 +17,16 @@ func characterAt(index: Int, _ array: [String]) -> Character {
 
 func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
     var str: String = string
-
     if str.count <= 0 {
         str.append(characterAt(index: 0, array))
     }
     else {
         str.replace(at: str.count - 1,
                     with: characterAt(index: (indexOf(character: str.last!, array) + 1) % array.count, array))
-
         if indexOf(character: str.last!, array) == 0 {
             str = String(generateBruteForce(String(str.dropLast()),
                                         fromArray: array)) + String(str.last!)
         }
     }
     return str
-}
-
-func bruteForce(passwordToUnlock: String) {
-    let ALLOWED_CHARACTERS: [String] = String().digitsAndLetters.map { String($0) }
-    var password: String = ""
-    while password != passwordToUnlock {
-        password = generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
-        print(password)
-    }
-    print(password)
 }
--- GCDSampleBruteForce/ViewController.swift
+++ GCDSampleBruteForce/ViewController.swift
@@ -10,7 +10,10 @@ import SnapKit
 
 class ViewController: UIViewController {
 
-    var password = ""
+    private var password = ""
+    private let queue = DispatchQueue.global(qos: .background)
+    private var workItem: DispatchWorkItem?
+    private var continueTask = true
 
     // MARK: - UI
 
@@ -74,6 +77,32 @@ class ViewController: UIViewController {
         return button
     }()
 
+    private lazy var bruteControlButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Stop brute", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(stopBrute), for: .touchUpInside)
+        button.backgroundColor = UIColor(red: 0.42, green: 0.45, blue: 0.81, alpha: 1.00)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 18
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.3
+        button.layer.shadowOffset = .zero
+        button.layer.shadowRadius = 10
+        button.layer.shouldRasterize = true
+        button.layer.rasterizationScale = UIScreen.main.scale
+        button.isHidden = true
+        return button
+    }()
+
+    private lazy var bruteLabel: UILabel = {
+        let label = UILabel()
+        label.text = ""
+        label.textColor = .green
+        label.font = UIFont.systemFont(ofSize: 20)
+        return label
+    }()
+
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
@@ -91,6 +120,8 @@ class ViewController: UIViewController {
         view.addSubview(generatePasswordButton)
         view.addSubview(bruteForceButton)
         view.addSubview(activityView)
+        view.addSubview(bruteLabel)
+        view.addSubview(bruteControlButton)
     }
 
     func setupHierarchy(){
@@ -98,39 +129,46 @@ class ViewController: UIViewController {
             make.top.equalTo(view.snp.top).offset(180)
             make.centerX.equalToSuperview()
         }
-
         passwordTextField.snp.makeConstraints { make in
             make.top.equalTo(label.snp.bottom).offset(30)
             make.centerX.equalToSuperview()
             make.width.equalTo(240)
             make.height.equalTo(40)
         }
-
-        activityView.snp.makeConstraints({ make in
+        activityView.snp.makeConstraints{ make in
             make.top.equalTo(passwordTextField.snp.top).offset(10)
             make.leading.equalTo(passwordTextField.snp.trailing).offset(10)
-        })
-
+        }
         generatePasswordButton.snp.makeConstraints { make in
             make.top.equalTo(passwordTextField.snp.bottom).offset(40)
             make.centerX.equalToSuperview()
             make.width.equalTo(90)
             make.height.equalTo(35)
         }
-
         bruteForceButton.snp.makeConstraints { make in
             make.top.equalTo(generatePasswordButton.snp.bottom).offset(20)
             make.centerX.equalToSuperview()
             make.width.equalTo(100)
             make.height.equalTo(35)
         }
-
+        bruteLabel.snp.makeConstraints { make in
+            make.top.equalTo(bruteForceButton.snp.bottom).offset(30)
+            make.centerX.equalToSuperview()
+        }
+        bruteControlButton.snp.makeConstraints { make in
+            make.top.equalTo(bruteLabel.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(120)
+            make.height.equalTo(35)
+        }
     }
 
     //MARK: - Action
 
     @objc func generatePassword() {
         password = ""
+        passwordTextField.text = ""
+        passwordTextField.isSecureTextEntry = true
         var passwordLength = 4
         let chars: [Character] = String().digitsAndLetters.map { Character(extendedGraphemeClusterLiteral: $0) }
         repeat {
@@ -138,19 +176,42 @@ class ViewController: UIViewController {
             passwordLength -= 1
         } while passwordLength > 0
         passwordTextField.insertText(password)
-
     }
 
     @objc func brutePassword() {
-        let queue = DispatchQueue.global(qos: .background)
+        continueTask = true
         let passwordToBrute = passwordTextField.text ?? ""
         activityView.startAnimating()
-        queue.async {
-            bruteForce(passwordToUnlock: passwordToBrute)
+        bruteControlButton.isHidden = false
+        workItem = DispatchWorkItem {
+            self.bruteForce(passwordToUnlock: passwordToBrute)
+            if self.continueTask {
+                DispatchQueue.main.sync {
+                    self.label.text = passwordToBrute
+                    self.passwordTextField.isSecureTextEntry = false
+                    self.activityView.stopAnimating()
+                    self.bruteControlButton.isHidden = true
+                }
+            }
+        }
+        queue.async(execute: workItem!)
+    }
+
+    @objc func stopBrute() {
+        continueTask = false
+        workItem?.cancel()
+        label.text = "Password not cracked: \(passwordTextField.text ?? password)"
+        activityView.stopAnimating()
+        bruteControlButton.isHidden = true
+    }
+
+    func bruteForce(passwordToUnlock: String) {
+        let ALLOWED_CHARACTERS: [String] = String().digitsAndLetters.map { String($0) }
+        var password: String = ""
+        while continueTask && password != passwordToUnlock {
+            password = generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
             DispatchQueue.main.sync {
-                self.label.text = self.password
-                self.passwordTextField.isSecureTextEntry = false
-                self.activityView.stopAnimating()
+                self.bruteLabel.text = password
             }
         }
     }
